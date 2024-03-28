9fa51497751dab39c1768528a98008298a603675
Aesothoth <90930838+Aesothoth@users.noreply.github.com>
2024-03-27 21:23:32 -0400

    update Program and Cell classes

--- Cell.cs
+++ Cell.cs
@@ -3,6 +3,7 @@ using CsvHelper;
 using CsvHelper.Configuration.Attributes;
 using System;
 using System.Text.RegularExpressions;
+using System.Runtime.CompilerServices;
 
 public class Cell 
 {
@@ -27,112 +28,100 @@ public class Cell
 
 
 
-	// Data Transformation Methods
-	public List<Cell> ReadCSVFile(string filePath, int num_entries)
-	{
-		List<Cell> cellList = new List<Cell>();
-
-		using (var reader = new StreamReader(filePath))
-		using (var csv = new CsvReader(reader, new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)))
-		{
-			csv.Context.RegisterClassMap<CellMap>();
-
-			for(int i = 0; i < num_entries; i++)
-			{
-				csv.Read();
-				Cell record = csv.GetRecord<Cell>();
-				CleanCell(record);
-				cellList.Add(record);
-			}
-		}
-		return cellList;
-	}
-	public void PrintCells(List<Cell> cellList)
-	{
-		for (int i = 0; i < cellList.Count; i++)
-		{
-			var cell = cellList[i];
-			Console.WriteLine($"Cell {i + 1}: OEM - {cell.oem}, Model - {cell.model}, Body Weight - {cell.body_weight_float}, Display Size - {cell.display_size_float}");
-			// Add additional properties as needed
-		}
-	}
-	private void CleanCell(Cell record)
+	// Data Transformation
+	public void CleanCell()
 	{
 		// Set each variable to null if it is empty or "-"
-		if (record.oem == "" || record.oem == " " || record.oem == "-")
+		if (this.oem == "" || this.oem == " " || this.oem == "-")
 		{
-			record.oem = null;
+			this.oem = null;
 		}
-		if (record.model == "" || record.model == " " || record.model == "-")
+		if (this.model == "" || this.model == " " || this.model == "-")
 		{
-			record.model = null;
+			this.model = null;
 		}
-		if (record.launch_announced == "" || record.launch_announced == " " || record.launch_announced == "-")
+		if (this.launch_announced == "" || this.launch_announced == " " || this.launch_announced == "-")
 		{
-			record.launch_announced = null;
+			this.launch_announced = null;
 		}
-		if (record.launch_status == "" || record.launch_status == " " || record.launch_status == "-")
+		if (this.launch_status == "" || this.launch_status == " " || this.launch_status == "-")
 		{
-			record.launch_status = null;
+			this.launch_status = null;
 		}
-		if (record.body_dimensions == "" || record.body_dimensions == " " || record.body_dimensions == "-")
+		if (this.body_dimensions == "" || this.body_dimensions == " " || this.body_dimensions == "-")
 		{
-			record.body_dimensions = null;
+			this.body_dimensions = null;
 		}
-		if (record.body_weight == "" || record.body_weight == " " || record.body_weight == "-")
+		if (this.body_weight == "" || this.body_weight == " " || this.body_weight == "-")
 		{
-			record.body_weight = null;
+			this.body_weight = null;
 		}
-		if (record.body_sim == "" || record.body_sim == " " || record.body_sim == "-")
+		if (this.body_sim == "" || this.body_sim == " " || this.body_sim == "-")
 		{
-			record.body_sim = null;
+			this.body_sim = null;
 		}
-		if (record.display_type == "" || record.display_type == " " || record.display_type == "-")
+		if (this.display_type == "" || this.display_type == " " || this.display_type == "-")
 		{
-			record.display_type = null;
+			this.display_type = null;
 		}
-		if (record.display_size == "" || record.display_size == " " || record.display_size == "-")
+		if (this.display_size == "" || this.display_size == " " || this.display_size == "-")
 		{
-			record.display_size = null;
+			this.display_size = null;
 		}
-		if (record.display_resolution == "" || record.display_resolution == " " || record.display_resolution == "-")
+		if (this.display_resolution == "" || this.display_resolution == " " || this.display_resolution == "-")
 		{
-			record.display_resolution = null;
+			this.display_resolution = null;
 		}
-		if (record.features_sensors == "" || record.features_sensors == " " || record.features_sensors == "-")
+		if (this.features_sensors == "" || this.features_sensors == " " || this.features_sensors == "-")
 		{
-			record.features_sensors = null;
+			this.features_sensors = null;
 		}
-		if (record.platform_os == "" || record.platform_os == " " || record.platform_os == "-")
+		if (this.platform_os == "" || this.platform_os == " " || this.platform_os == "-")
 		{
-			record.platform_os = null;
+			this.platform_os = null;
 		}
 
 		// Check date format
 		Regex regex = new Regex(@"\b\d{4}\b|Discontinued|Cancelled");
-		if(record.launch_announced != null && !regex.IsMatch(record.launch_announced))
+		if(this.launch_announced != null && !regex.IsMatch(this.launch_announced))
 		{
-			//Console.WriteLine($"Invalid announcement date for phone of model: {record.model}");
-			//Console.WriteLine(record.launch_announced);
-			record.launch_announced = null;
-		}
-		else if(record.launch_announced != null)
-		{
-			record.launch_announced_int = int.Parse(record.launch_announced.Substring(0,4));
-			//Console.WriteLine(record.launch_announced_int);
-		}
-		if(record.launch_status != null && record.launch_status != "Discontinued" && record.launch_status != "Cancelled" && !regex.IsMatch(record.launch_status))
-		{
-			Console.WriteLine($"Invalid launch status for phone of model: {record.model}");
-			record.launch_status = null;
+			//Console.WriteLine($"Invalid announcement date for phone of model: {this.model}");
+			//Console.WriteLine(this.launch_announced);
+			this.launch_announced = null;
 		}
 
-		// Find the year an item was/will be released
-		Regex regex1 = new Regex(@"");
-		if(record.launch_status != null && record.launch_status != "Discontinued" && record.launch_status != "Cancelled")
+		// Change date to integer for calculations
+		try
 		{
-			string numberPattern = @"\d\d\d\d";
-			MatchCollection matches = Regex.Matches(record.launch_status, numberPattern);
+			MatchCollection matches = Regex.Matches(this.launch_announced, @"\b\d{4}\b");
+			foreach (Match match in matches)
+			{
+				if (match.Success && match.Value.Length >= 4)
+				{
+					if (int.TryParse(match.Value.Substring(0, 4), out int year))
+					{
+						this.launch_announced_int = year;
+					}
+				}
+			}
+		} catch 
+		{
+			Console.WriteLine("launch_announced is null");
+		}
+
+
+		if (this.launch_status != "Discontinued" && this.launch_status != "Cancelled" && !regex.IsMatch(this.launch_status))
+		{
+			Console.WriteLine($"Invalid launch status for phone of model: {this.model}");
+			this.launch_status = null;
+		}
+
+
+		// Find the year an item was/will be released
+		if(this.launch_status != null && this.launch_status != "Discontinued" && this.launch_status != "Cancelled")
+		{
+			string pattern = @"\d\d\d\d";
+			MatchCollection matches = Regex.Matches(this.launch_status, pattern);
 
 			foreach (Match match in matches)
 			{
@@ -140,7 +129,7 @@ public class Cell
 				{
 					if (int.TryParse(match.Value.Substring(0, 4), out int year))
 					{
-						record.launch_year = year;
+						this.launch_year = year;
 					}
 				}
 			}
@@ -148,58 +137,58 @@ public class Cell
 
 		// Convert body_weight
 		regex = new Regex(@"(\d+(\.\d+)?)\s*g");
-		if(record.body_weight != null && !regex.IsMatch(record.body_weight))
+		if(this.body_weight != null && !regex.IsMatch(this.body_weight))
 		{
-			//Console.WriteLine($"Invalid body weight for phone of model: {record.model}");
-			record.body_weight = null;
+			//Console.WriteLine($"Invalid body weight for phone of model: {this.model}");
+			this.body_weight = null;
 		}
-		else if(record.body_weight != null)
+		else if(this.body_weight != null)
 		{
-			int index = record.body_weight.IndexOfAny(new char[] { ' ', 'g' });
+			int index = this.body_weight.IndexOfAny(new char[] { ' ', 'g' });
 			if(index > 0) 
 			{
-				string weightString = record.body_weight.Substring(0, index);
-				record.body_weight_float = float.Parse(weightString);
+				string weightString = this.body_weight.Substring(0, index);
+				this.body_weight_float = float.Parse(weightString);
 			}
 		}
 
 		// Check body_sim validity
-		if(record.body_sim == "No" || record.body_sim == "Yes" || record.body_sim == "no" || record.body_sim == "yes")
+		if(this.body_sim == "No" || this.body_sim == "Yes" || this.body_sim == "no" || this.body_sim == "yes")
 		{
-			//Console.WriteLine($"Invalid body sim for phone of model: {record.model}");
-			record.body_sim = null;
+			//Console.WriteLine($"Invalid body sim for phone of model: {this.model}");
+			this.body_sim = null;
 		}
 
 		// Convert display_size
 		regex = new Regex(@"(\d+(\.\d+)?) inches");
-		if (record.display_size != null && !regex.IsMatch(record.display_size))
+		if (this.display_size != null && !regex.IsMatch(this.display_size))
 		{
-			//Console.WriteLine($"Invalid display size for phone of model: {record.model}");
-			record.display_size = null;
+			//Console.WriteLine($"Invalid display size for phone of model: {this.model}");
+			this.display_size = null;
 		}
-		else if (record.display_size != null)
+		else if (this.display_size != null)
 		{
-			int index = record.display_size.IndexOfAny(new char[] { ' ', 'i' });
+			int index = this.display_size.IndexOfAny(new char[] { ' ', 'i' });
 			if (index > 0)
 			{
-				string sizeString = record.display_size.Substring(0, index);
-				record.display_size_float = float.Parse(sizeString);
+				string sizeString = this.display_size.Substring(0, index);
+				this.display_size_float = float.Parse(sizeString);
 
 			}
 		}
 
 		// Check features_sensors validity
-		if (record.features_sensors != null && float.TryParse(record.features_sensors, out _))
+		if (this.features_sensors != null && float.TryParse(this.features_sensors, out _))
 		{
-			//Console.WriteLine($"Invalid features sensors for phone of model: {record.model}");
-			record.features_sensors = null;
+			//Console.WriteLine($"Invalid features sensors for phone of model: {this.model}");
+			this.features_sensors = null;
 		}
 
 		// Check platform_os validity
-		if (record.platform_os != null && float.TryParse(record.platform_os, out _))
+		if (this.platform_os != null && float.TryParse(this.platform_os, out _))
 		{
-			//Console.WriteLine($"Invalid platform os for phone of model: {record.model}");
-			record.platform_os = null;
+			//Console.WriteLine($"Invalid platform os for phone of model: {this.model}");
+			this.platform_os = null;
 		}
 
 	}
--- Program.cs
+++ Program.cs
@@ -5,9 +5,19 @@ using System.IO;
 using CsvHelper;
 using CsvHelper.Configuration;
 
-class Program
+class Program : Cell
 {
-	public void ReadCSVFile(string filePath)
+	static void Main()
+	{
+		Program program = new Program();
+		Console.WriteLine("How many entries would you like to read?: ");
+		int num_emtries = Convert.ToInt32(Console.ReadLine());
+		List<Cell> cells;
+		cells = program.ReadCSVFile("C:\\Users\\Aes\\Desktop\\Fall_2024\\Programming Languages\\ALP3\\ALP\\cells.csv", num_emtries);
+		//program.PrintCells(cells);
+	}
+
+	public List<Cell> ReadCSVFile(string filePath, int num_entries)
 	{
 		List<Cell> cellList = new List<Cell>();
 
@@ -16,29 +26,23 @@ class Program
 		{
 			csv.Context.RegisterClassMap<CellMap>();
 
-			while (csv.Read())
+			for (int i = 0; i < num_entries; i++)
 			{
-				var record = csv.GetRecord<Cell>();
+				csv.Read();
+				Cell record = csv.GetRecord<Cell>();
+				record.CleanCell();
 				cellList.Add(record);
 			}
 		}
-
-		PrintFirst10Cells(cellList);
+		return cellList;
 	}
-
-	public void PrintFirst10Cells(List<Cell> cellList)
+	public void PrintCells(List<Cell> cellList)
 	{
 		for (int i = 0; i < cellList.Count; i++)
 		{
 			var cell = cellList[i];
-			Console.WriteLine($"Cell {i + 1}: OEM - {cell.oem}, Model - {cell.model}, ");
+			Console.WriteLine($"Cell {i + 1}: OEM - {cell.oem}, Model - {cell.model}, Body Weight - {cell.body_weight_float}, Display Size - {cell.display_size_float}");
 			// Add additional properties as needed
 		}
-	}
-
-	static void Main()
-	{
-		Program program = new Program();
-		program.ReadCSVFile("C:\\Users\\Aes\\Desktop\\Fall_2024\\Programming Languages\\ALP3\\ALP\\cells.csv");
 	}
 }
